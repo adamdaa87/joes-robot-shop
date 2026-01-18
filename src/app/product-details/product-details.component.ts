61ca9a7a6b22143fbbab78fb7fc71d20813f1515
Adam Daa <adamdaa87@gmail.com>
2026-01-18 23:09:50 +0100

    Merge branch 'main' of https://github.com/adamdaa87/joes-robot-shop

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IProduct } from '../product.model';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'bot-product-details',
+  imports: [CurrencyPipe],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
+})
+export class ProductDetailsComponent {
+  product: IProduct;
+
+  constructor() {
+    this.product = {
+      id: 1,
+      description:
+        'A frindly robot head with two eyes and a smile -- great for domestic use.',
+      name: 'Friendly Bot',
+      imageName: 'head-friendly.png',
+      category: 'Heads',
+      price: 945.0,
+      discount: 0.2,
+    };
+  }
+}
